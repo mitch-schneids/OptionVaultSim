1f04b70d636e52df34faa3a8e577cda47c9325a4
mitch-schneids <103954720+mitch-schneids@users.noreply.github.com>
2022-07-05 10:26:48 -0500

    Update model.py

--- src/model.py
+++ src/model.py
@@ -27,7 +27,7 @@ def main():
     
     strike_prices = [4400, 3700, 3700, 3200, 2800]
     
-    for _ in range(100):
+    for _ in range(50):
         params = generate_params()
         week_cnt = 0
 
@@ -46,83 +46,88 @@ def main():
                 'strike_price': strike_prices[week_cnt],
                 'premium': 0,
                 'options_sold' : 0,
-                'expired' : False
+                'expired' : False,
+                'dummy' : True
             }
 
-            PSUBs = {
-                'agg_state' : {
+            PSUBs = [
+                { # agg fee
                     'policies' : {},
                     'variables' : {
-                        's_fee_agg' : su.s_fee_agg,
-                        's_deposit_agg' : su.s_deposit_agg
-                    }
+                        's1' : su.s_fee_agg,
+                        's2' : su.s_deposit_agg,
+                                       }
                 },
-                'dov_state0' : {
+                { # dov deposits
                     'policies' : {},
                     'variables' : {
-                        's_deposit_dov' : su.s_deposit_dov,
-                        's_options_expired' : su.s_options_expired
+                        's1' : su.s_deposit_dov,
+                        's2' : su.s_options_expired
                     }
                 },
 
-                'dov_state1' : {
+                { # options sold
                     'policies' : {},
                     'variables' : {
-                        's_options_sold' : su.s_options_sold
-                    }
+                        's1' : su.s_options_sold,
+                        's2' : su.s_dummy
+                       }
                 },
-                'dov_state2' : {
+                { #dov premium and fees
                     'policies' : {},
                     'variables' : {
-                        's_premium_dov' : su.s_premium_dov,
-                        's_fee_dov' : su.s_fee_dov
-                    }
+                        's1' : su.s_premium_dov,
+                        's2' : su.s_fee_dov,
+                      }
                 }
-            }
+            ]
 
 
-        pd.options.plotting.backend = 'plotly'
+            pd.options.plotting.backend = 'plotly'
 
-        MONTE_CARLO_RUNS = 50
-        SIMULATION_TIMESTEPS = 500
+            MONTE_CARLO_RUNS = 50
+            SIMULATION_TIMESTEPS = 500
 
-        print(params)
-        sim_config = config_sim(
-            {
-            'N': MONTE_CARLO_RUNS,
-            'T': range(SIMULATION_TIMESTEPS),
-            'M': params
-            }
-        )
+            print(params)
+            print()
+            sim_config = config_sim(
+                {
+                'N': 5,
+                'T': range(1),
+                'M': params
+                }
+            )
 
-        # Append configs
-        exp = Experiment()
-        exp.append_configs(
-            initial_state = genesis_states, 
-            partial_state_update_blocks = PSUBs, 
-            sim_configs = sim_config
+            # Append configs
+            exp = Experiment()
+            exp.append_configs(
+                initial_state = genesis_states, 
+                partial_state_update_blocks = PSUBs, 
+                sim_configs = sim_config
 
-        )
-        # Execution
-        exec_mode = ExecutionMode()
-        local_mode_ctx = ExecutionContext(context=exec_mode.local_mode)
-        simulation = Executor(exec_context=local_mode_ctx, configs=exp.configs)
+            )
+            # Execution
+            exec_mode = ExecutionMode()
+            local_mode_ctx = ExecutionContext(context=exec_mode.local_mode)
+            simulation = Executor(exec_context=local_mode_ctx, configs=exp.configs)
 
-        raw_system_events, tensor_field, sessions = simulation.execute()
+            raw_system_events, tensor_field, sessions = simulation.execute()
 
-        simulation_result = pd.DataFrame(raw_system_events)
+            simulation_result = pd.DataFrame(raw_system_events)
+            
+            print(simulation_result)
 
-        df = simulation_result.copy()
-        df = df[df.simulation == 0]
-        df
+            # print(raw_system_events)
+            # df = simulation_result.copy()
+            # df = df[df.simulation == 0]
 
-        df[df.run == 1].head()
-        df[df.run == 2].head()
-        df[df.run == 3].head()
+            # df[df.run == 1].head()
+            # df[df.run == 2].head()
+            # df[df.run == 3].head()
 
-        px.line(df, x='timestep', y=['agg_fee', 'dov_fee', 'premium'], facet_row = 'subset', height=800, width=1000)
+            # px.line(df, x='timestep', y=['agg_fee', 'dov_fee', 'premium'], facet_row = 'subset', height=800, width=1000)
 
-        week_cnt += 1
+            week_cnt += 1
 
 if __name__ == '__main__':
     main()
